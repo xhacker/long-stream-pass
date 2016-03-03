4f528fbdbd60fb391660e58ffea6692e63abcd6d
柳东原 LIU Dongyuan <liu.dongyuan@gmail.com>
2016-03-03 15:22:16 -0800

    Add function and call

--- add128-opt.ll
+++ add128-opt.ll
@@ -5,10 +5,13 @@
 declare i32 @printf(i8*, ...)
 
 define i32 @main() {
+  %1 = call i128 @add128(i128 24376736269128437184639567244956479712, i128 57643874189567368763458875368743675743)
   %result = add i128 24376736269128437184639567244956479712, 57643874189567368763458875368743675743
   %lower64 = trunc i128 %result to i64
-  %result_shifted = lshr i128 %result, 64
+  %result_shifted = lshr i128 %1, 64
   %higher64 = trunc i128 %result_shifted to i64
-  %1 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([16 x i8], [16 x i8]* @.str, i32 0, i32 0), i64 %higher64, i64 %lower64)
+  %2 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([16 x i8], [16 x i8]* @.str, i32 0, i32 0), i64 %higher64, i64 %lower64)
   ret i32 0
 }
+
+declare i128 @add128(i128, i128)
--- longstream/Longstream.cpp
+++ longstream/Longstream.cpp
@@ -1,8 +1,11 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
@@ -12,7 +15,41 @@ namespace {
     LongstreamPass() : ModulePass(ID) {}
 
     virtual bool runOnModule(Module &m) {
-      errs() << "Holy Crap!!!\n";
+      llvm::LLVMContext &c = m.getContext();
+      Constant *cons = m.getOrInsertFunction("add128",
+                                             Type::getIntNTy(c, 128),
+                                             Type::getIntNTy(c, 128),
+                                             Type::getIntNTy(c, 128),
+                                             NULL);
+      Function *func = cast<Function>(cons);
+
+      IRBuilder<> functionBuilder(c);
+      Function *main = m.getFunction("main");
+      Function::iterator b = main->begin();
+      BasicBlock::iterator bi = b->begin();
+      functionBuilder.SetInsertPoint(b, bi);
+
+      for (Function &f : m) {
+        for (BasicBlock &bb : f) {
+          for (Instruction &inst : bb) {
+            if (inst.getOpcode() == Instruction::Add) {
+              Value *op0 = inst.getOperand(0);
+              Value *op1 = inst.getOperand(1);
+
+              Type *type = op0->getType();
+              if (type->isIntegerTy(128)) {
+                IRBuilder<> builder(&inst);
+                Value *call = builder.CreateCall(func, {op0, op1}, "");
+
+                for (auto &u : inst.uses()) {
+                  User *user = u.getUser();
+                  user->setOperand(u.getOperandNo(), call);
+                }
+              }
+            }
+          }
+        }
+      }
       return true;
     }
   };
